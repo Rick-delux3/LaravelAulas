dd88af41c3c9acf94c79c8c92f8e123babe47416
Rick-delux3 <henriquerodriguesneves4@gmail.com>
2026-08-04 17:51:26 -0300

    Orm

---
+++ app/Models/AlunoModel.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AlunoModel extends Model
+{
+    use HasFactory;
+
+    private $table = 'aluno';
+
+    
+
+
+}
