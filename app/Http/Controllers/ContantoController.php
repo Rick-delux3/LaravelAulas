d6aab4693b8e02d97fba83dd6ad5458194a1e6b9
Rick-delux3 <henriquerodriguesneves4@gmail.com>
2026-09-01 14:22:12 -0300

    seeders

---
+++ app/Http/Controllers/ContantoController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ContantoController extends Controller
+{
+    public function principal()
+    {
+        echo 'contato';
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -12,7 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(FornecedorSeeder::class);
+        //$this->call(FornecedorSeeder::class);
         $this->call(ContatoSeeder::class);
     }
 }
